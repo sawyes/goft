6af4ec53086f5c0ea7bbefef2b08d50d5a17fbdd
v_bianjhwu <v_bianjhwu@tencent.com>
2021-02-08 01:57:23 +0800

    add yaml config

--- src/goft/SysConfig.go
+++ src/goft/SysConfig.go
@@ -1,6 +1,7 @@
 package goft
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"log"
 )
@@ -16,16 +17,23 @@ type SysConfig struct {
 }
 
 func NewSysConfig() *SysConfig {
+	// 默认配置
 	return &SysConfig{Server: &ServerConfig{Name: "myweb", Port: 8080}}
 }
 
+// 初始化配置, 存在配置文件则用配置, 否则加载默认
 func InitConfig() *SysConfig  {
 	conf:=NewSysConfig()
 	if b:= LoadConfigFile();b!=nil {
+		
 		err := yaml.Unmarshal(b, conf)
 		if err!=nil {
+			log.Println("SysConfig load yaml failed")
 			log.Fatal(err)
 		}
+		
+		log.Println("SysConfig load success")
+		log.Println(fmt.Sprintf("%+v", *conf.Server))
 	}
 	return conf
 }
